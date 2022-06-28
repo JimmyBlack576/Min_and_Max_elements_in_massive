5df63141ecb720da1de7d1a56fdbc240c42e8f92
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-06-29 00:25:41 +0300

    Добавьте файлы проекта.

---
+++ Min_and_Max_elements_in_massive/Min_and_Max_elements_in_massive.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    const size_t s = 10;
+    int arr[s] = {};
+    int max, min;
+    for (int i = 0; i < s; i++) {
+        arr[i] = rand() % 9 + 1;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    max = arr[0], min = arr[0];
+    for (int j = 0; j < s; j++) {
+        if (arr[j] > max) {
+             max = arr[j] ;
+        }
+        else if (arr[j] < min) {
+             min = arr[j] ;
+        }
+
+    }
+    cout <<"Max = "<< max << " " <<"Min = "<< min;
+}
+
